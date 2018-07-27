1d4a2908ebf8309ed037e54e77f8221e2f3ca1e9
闫亚松 <815801759@qq.com>
2018-07-27 19:07:29 +0800

    tst

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -28,7 +28,7 @@ class WeChatController extends Controller
 
     function getConfig(){
         $app = app('wechat.official_account');
-        $url = $_SERVER['REQUEST_URI'];
+        $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
         $app->jssdk->setUrl($url);
         $result = $app->jssdk->buildConfig([
             'onMenuShareAppMessage',
