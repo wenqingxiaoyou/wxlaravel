54bbe783b31eb3cf41fc57960e2bb94ed19c2a35
闫亚松 <815801759@qq.com>
2018-07-28 10:26:12 +0800

    test

--- resources/views/introduce/introduce.blade.php
+++ resources/views/introduce/introduce.blade.php
@@ -62,6 +62,7 @@
 <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
 
+    var  aaa = 1;
    $.ajax({
        url:'/getwxconfig',
        success:function (res) {
